df63ed8f73769c5c3cf2e82a7efe0fadcbb99d2b
arbi <arbidaci@gmail.com>
2021-02-22 19:56:17 -0500

    exmplemented bar graph that populates with each label being pressed, responsive elements, css styling

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ d3.json("data.json").then(function (data) {
   d3.select("div#gaugeData")
     .selectAll("a")
     .data(data.gaugeData)
-    // // .remove()
+    // // .remove().enter()
     // .enter()
     // .append("a")
     // .attr("href", "#")
@@ -23,70 +23,180 @@ d3.json("data.json").then(function (data) {
 
 function areaDataChart(areaChart) {
   if (areaChart == "qs") {
-    d3.json("data.json").then(function (data) {
-      d3.select("div#areaData")
-        .selectAll("p")
-        .remove()
-        .data(data.areaData.QualityScore)
-        .enter()
-        .append("p")
-        .text((dta) => dta.date + dta.score);
-    });
-  } else if (areaChart == "b") {
-    d3.json("data.json").then(function (data) {
-      d3.select("div#areaData")
-        .selectAll("p")
-        .remove()
-        .data(data.areaData.Basics)
-        .enter()
-        .append("p")
-        .text((dta) => dta.date + dta.score);
-    });
-  } else if (areaChart == "i") {
-    d3.json("data.json").then(function (data) {
-      d3.select("div#areaData")
-        .selectAll("p")
-        .remove()
-        .data(data.areaData.Interaction)
-        .enter()
-        .append("p")
-        .text((dta) => dta.date + dta.score);
-    });
-  } else if (areaChart == "e") {
-    d3.json("data.json").then(function (data) {
-      d3.select("div#areaData")
-        .selectAll("p")
-        .remove()
-        .data(data.areaData.Expertise)
-        .enter()
-        .append("p")
-        .text((dta) => dta.date + dta.score);
-    });
-  } else if (areaChart == "p") {
-    d3.json("data.json").then(function (data) {
-      d3.select("div#areaData")
-        .selectAll("p")
-        .remove()
-        .data(data.areaData.Process)
-        .enter()
-        .append("p")
-        .text((dta) => dta.date + dta.score);
-    });
-  } else if (areaChart == "k") {
-    d3.json("data.json").then(function (data) {
-      d3.select("div#areaData")
-        .selectAll("p")
-        .remove()
-        .data(data.areaData.Knowledge)
-        .enter()
-        .append("p")
-        .text((dta) => dta.date + dta.score);
-    });
-  }else if (areaChart == "reset"){
+    //
+    d3.json("/data/data.json").then(function(data){
+  
+      const xScale = 
+      d3.scaleBand()
+      .domain(data.areaData.QualityScore.map((dataPoint)=>dataPoint.date))
+      .rangeRound([0,500])
+      .padding(0.1);
+    
+      const yScale = 
+      d3.scaleLinear()
+      .domain([0,100])
+      .range([300,0]);
+      
+      d3.select("svg#areaData")
+      .selectAll('.bar')
+      .remove().enter()
+      .data(data.areaData.QualityScore)
+      .enter()
+      .append('rect')
+      .classed('bar', true)
+      .attr('width',xScale.bandwidth())
+      .attr('height', (data)=>300-yScale(data.score))
+      .attr('x', data=>xScale(data.date))
+      .attr('y', data=>yScale(data.score));
+    })
+  } 
+
+  else if (areaChart == "b") {
+    d3.json("/data/data.json").then(function(data){
+  
+      const xScale = 
+      d3.scaleBand()
+      .domain(data.areaData.Basics.map((dataPoint)=>dataPoint.date))
+      .rangeRound([0,500])
+      .padding(0.1);
+    
+      const yScale = 
+      d3.scaleLinear()
+      .domain([0,100])
+      .range([300,0]);
+      
+      d3.select("svg#areaData")
+      .selectAll('.bar')
+      .remove().enter()
+      .data(data.areaData.Basics)
+      .enter()
+      .append('rect')
+      .classed('bar', true)
+      .attr('width',xScale.bandwidth())
+      .attr('height', (data)=>300-yScale(data.score))
+      .attr('x', data=>xScale(data.date))
+      .attr('y', data=>yScale(data.score));
+    })
+  } 
+  
+  else if (areaChart == "i") {
+    d3.json("/data/data.json").then(function(data){
+  
+      const xScale = 
+      d3.scaleBand()
+      .domain(data.areaData.Interaction.map((dataPoint)=>dataPoint.date))
+      .rangeRound([0,500])
+      .padding(0.1);
+    
+      const yScale = 
+      d3.scaleLinear()
+      .domain([0,100])
+      .range([300,0]);
+      
+      d3.select("svg#areaData")
+      .selectAll('.bar')
+      .remove().enter()
+      .data(data.areaData.Interaction)
+      .enter()
+      .append('rect')
+      .classed('bar', true)
+      .attr('width',xScale.bandwidth())
+      .attr('height', (data)=>300-yScale(data.score))
+      .attr('x', data=>xScale(data.date))
+      .attr('y', data=>yScale(data.score));
+    })
+  } 
+  
+  else if (areaChart == "e") {
+    d3.json("/data/data.json").then(function(data){
+  
+      const xScale = 
+      d3.scaleBand()
+      .domain(data.areaData.Expertise.map((dataPoint)=>dataPoint.date))
+      .rangeRound([0,500])
+      .padding(0.1);
+    
+      const yScale = 
+      d3.scaleLinear()
+      .domain([0,100])
+      .range([300,0]);
+      
+      d3.select("svg#areaData")
+      .selectAll('.bar')
+      .remove().enter()
+      .data(data.areaData.Expertise)
+      .enter()
+      .append('rect')
+      .classed('bar', true)
+      .attr('width',xScale.bandwidth())
+      .attr('height', (data)=>300-yScale(data.score))
+      .attr('x', data=>xScale(data.date))
+      .attr('y', data=>yScale(data.score));
+    })
+  } 
+  
+  else if (areaChart == "p") {
+    d3.json("/data/data.json").then(function(data){
+  
+      const xScale = 
+      d3.scaleBand()
+      .domain(data.areaData.Process.map((dataPoint)=>dataPoint.date))
+      .rangeRound([0,500])
+      .padding(0.1);
+    
+      const yScale = 
+      d3.scaleLinear()
+      .domain([0,100])
+      .range([300,0]);
+      
+      d3.select("svg#areaData")
+      .selectAll('.bar')
+      .remove().enter()
+      .data(data.areaData.Process)
+      .enter()
+      .append('rect')
+      .classed('bar', true)
+      .attr('width',xScale.bandwidth())
+      .attr('height', (data)=>300-yScale(data.score))
+      .attr('x', data=>xScale(data.date))
+      .attr('y', data=>yScale(data.score));
+    })
+  } 
+  
+  else if (areaChart == "k") {
+    d3.json("/data/data.json").then(function(data){
+      
+      const xScale = 
+      d3.scaleBand()
+      .domain(data.areaData.Knowledge.map((dataPoint)=>dataPoint.date))
+      .rangeRound([0,500])
+      .padding(0.1);
+    
+      const yScale = 
+      d3.scaleLinear()
+      .domain([0,100])
+      .range([300,0]);
+      
+      d3.select("svg#areaData")
+      .selectAll('.bar')
+      .remove().enter()
+      .data(data.areaData.Knowledge)
+      .enter()
+      .append('rect')
+      .classed('bar', true)
+      .attr('width',xScale.bandwidth())
+      .attr('height', (data)=>300-yScale(data.score))
+      .attr('x', data=>xScale(data.date))
+      .attr('y', data=>yScale(data.score));
+    })
+  }
+  
+  else if (areaChart == "reset"){
     d3.select("div#areaData")
     .selectAll("p")
-    .remove()
+    .remove().enter()
     .exit()
   }
 }
+
 
